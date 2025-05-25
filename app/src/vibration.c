a646bf5f6036e10eec49966f88ae61093249c987
peopleAlreadyKnowWhoIAm <nz54ds@gmail.com>
2025-05-25 14:54:56 +0300

    Made changes required for vibration motor integration

--- app/src/vibration.c
+++ app/src/vibration.c
@@ -2,7 +2,7 @@
 
 #include <zephyr/drivers/gpio.h>
 
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+static const struct gpio_dt_spec vibration_control = GPIO_DT_SPEC_GET(DT_NODELABEL(vibration_output), gpios);
 static bool is_vibrating = false;
 
 static void vibration_work_handler(struct k_work *) {
@@ -16,7 +16,7 @@ void vibration_start(void) {
         return;
     }
     is_vibrating = true;
-   (void)gpio_pin_set_dt(&led, 1); 
+   (void)gpio_pin_set_dt(&vibration_control, 1); 
 }
 
 void vibration_short_start(void) {
@@ -24,7 +24,7 @@ void vibration_short_start(void) {
         return;
     }
     is_vibrating = true;
-   gpio_pin_set_dt(&led, 1);
+   gpio_pin_set_dt(&vibration_control, 1);
    k_work_schedule(&stop_vibration_work, K_MSEC(VIBRATION_SHORT_DURATION));
    is_vibrating = false;
 }
@@ -34,15 +34,15 @@ void vibration_stop(void) {
         return;
     }
     is_vibrating = false;
-   gpio_pin_set_dt(&led, 0);
+   gpio_pin_set_dt(&vibration_control, 0);
 }
 
 static int vibration_gpio_init(void) {
-    if (!device_is_ready(led.port)) {
+    if (!device_is_ready(vibration_control.port)) {
         return -1;
     }
 
-    int ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
+    int ret = gpio_pin_configure_dt(&vibration_control, GPIO_OUTPUT_INACTIVE);
     return ret;
 }
 SYS_INIT(vibration_gpio_init, APPLICATION, 1);
